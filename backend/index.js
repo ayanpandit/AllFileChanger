f0b5c085cdfd59569a4277d46c518c59287d74fe
AYAN PANDEY <aayanpandey8528@gmail.com>
2025-08-03 22:21:04 +0530

    remover the duplicate const fs = require('fs');

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
 const fs = require('fs');
-const fs = require('fs');
 const path = require('path');
 const { PDFDocument } = require('pdf-lib');
 
